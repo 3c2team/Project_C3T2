f631fec7735347440863b88e56cf92cd7a0c381a
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-11-06 12:25:02 +0900

    신혜리 storeController 리뷰 저장경로 수정

--- Project_Class3T2/src/main/java/com/itwillbs/c3t2/controller/StoreController.java
+++ Project_Class3T2/src/main/java/com/itwillbs/c3t2/controller/StoreController.java
@@ -40,7 +40,6 @@ public class StoreController {
 	@Autowired
 	private StoreService service;
 	
-	
 	@GetMapping("OnlineStore")
 	public String main(Model model) {
 		List<ProductVO> bestList = service.getProductbestList();
@@ -58,6 +57,7 @@ public class StoreController {
 	@GetMapping("StoreBest")
 	public String best(Model model) {
 		List<ProductVO> bestList = service.getProductbestList();
+//		System.out.println(bestList);
 		
 		model.addAttribute("bestList", bestList);
 		return "store/store_best";
@@ -101,12 +101,15 @@ public class StoreController {
 			case "ets": category_num = 4; break;
 		}
 		
-		if(category_num > 0 && category_num <= 4) {
+		System.out.println("int cate_num: " + category_num);
+		System.out.println("Sting cate: " + category);
+	
+		if(category_num > 0) {
 			proList = service.getProductCategoryList(category_num);
 		}
-		
 		model.addAttribute("proList", proList);
 		return "store/store_kit";
+		
 	}
 	
 	// 상세페이지~
@@ -159,16 +162,34 @@ public class StoreController {
 		return "store/product_detail";
 	}
 	
-	// 리뷰작성폼으로 이동
+//	@ResponseBody
+//	@PostMapping("ProductDetailFavorite")
+//	public void favorite(FavoriteVO favorite, HttpSession session, HttpServletResponse response) {
+//		int insertCount = service.registFavorite(favorite);
+//		
+//		if(insertCount > 0) {
+//			//찜하기 실패
+//		}
+//	}
+	
+	// 리뷰작성폼으로 이동~
 	@GetMapping("ReviewFrom")
 	public String reviewFrom(int proNum, HttpSession session, Model model) {
 		ProductVO product = service.getProductDetail(proNum);
 		String id = (String)session.getAttribute("sId");
 		
+		
+		// 로그인X 경우 close 처리
 		if(id == null || id.equals("")) {
 			model.addAttribute("msg", "리뷰를 작성하시려면 로그인해주세요.");
 			return "store/popup/close";
 		}
+		
+		// 상품구매X 경우 close 처리
+//		if(조건) {
+//			model.addAttribute("msg", "상품을 구매했을 경우만 리뷰등록이 가능합니다");
+//			return "store/popup/close";
+//		}
 		
 		model.addAttribute("product", product);
 		return "store/popup/review_from";
@@ -180,20 +201,27 @@ public class StoreController {
 							,@RequestParam(value = "file", required=false) MultipartFile file
 							, HttpSession session, Model model) {
 		
+		
 		String sId = (String)session.getAttribute("sId");
 		String uploadDir = "/review_img/"; //가상 경로
 		String saveDir = session.getServletContext().getRealPath(uploadDir).replace("Project_Class3T2/", ""); //실제 경로
 		
+		
+		
+		// 로그인X 처리
 		if(session.getAttribute("sId") == null) {
 			model.addAttribute("msg", "잘못된 접근입니다!");
-			return "sotre/popup/close";
+			return "store/popup/close";
 		}
 		
-		//===================== < 이미지 처리 > ===================== 
-		//--------------------- < 이미지 경로 > ---------------------
+//		//===================== < 이미지 처리 > ===================== 
+//		//--------------------- < 이미지 경로 > ---------------------
 		
+//		if(file.getName().equals("") || file == null) {
+//			map.put(uploadDir, saveDir)
+//		}
 		
-		// 서브디렉토리명 저장 yyyy/MM/dd 형식
+//		// 서브디렉토리명 저장 yyyy/MM/dd 형식
 		String subDir = "";
 		try {
 			LocalDate now = LocalDate.now();
@@ -208,24 +236,28 @@ public class StoreController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		//-------------------- < 이미지명 처리 > --------------------
+//		//-------------------- < 이미지명 처리 > --------------------
 		String uuid = UUID.randomUUID().toString();
-		System.out.println("uuid" + uuid);
-		
 		String fileName =  UUID.randomUUID().toString().substring(0, 3) + "_" + file.getOriginalFilename();
 		
-		if(file.getOriginalFilename() != null || !(file.getOriginalFilename().equals(""))) {
+		if(file == null || file.getOriginalFilename().equals("")) {
+			map.put("file_name", "");
+		} else {
+			map.put("file_name", uploadDir + subDir + "/" + fileName);
 		}
-		map.put("file_name", subDir + "/" + fileName);
 		map.put("member_id", sId);
 		
-		//------------------ < 게시물 등록 처리 > -------------------
+//		//------------------ < 게시물 등록 처리 > -------------------
 		
+//		System.out.println(review);
 		int insertCount = service.registReview(map);
+//		
 		try {
 			if(insertCount > 0) { //성공
-				System.out.println("리뷰등록 완료");
-				file.transferTo(new File(saveDir, fileName));
+				if(file != null && !(file.getOriginalFilename().equals(""))) {
+//					System.out.println("파일등록성공");
+					file.transferTo(new File(saveDir, fileName));
+				}
 				model.addAttribute("msg", "리뷰를 등록했습니다.");
 				
 				return "store/popup/close";
@@ -276,24 +308,5 @@ public class StoreController {
 			return "stoer/popup/close";
 		}
 		
-	}
-	
-	
-	
-	public enum ProductCategory {
-		STEAK(1),
-	    PASTA(2),
-	    SOUP(3),
-	    ETS(4);
-
-		private final int categoryCode;
-
-	    ProductCategory(int categoryCode) {
-    		this.categoryCode = categoryCode;
-	    }
-
-	    public int getCategoryCode() {
-	    	return categoryCode;
-	    }
 	}
 }
