0610c786787abeda7ee18ea0860de1034cab512b
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-11-08 19:19:02 +0900

    reservation 수정 - 은지

--- Project_Class3T2/src/main/webapp/resources/js/reservation.js
+++ Project_Class3T2/src/main/webapp/resources/js/reservation.js
@@ -4,19 +4,20 @@
  
 // ------------ 변수 --------------
 let nowDate = new Date();
-let thisMonth = new Date(nowDate.getFullYear(), nowDate.getMonth(), 1);
-let thisLastDay = new Date(nowDate.getFullYear(), nowDate.getMonth() + 1, 0);
 // --------------------------------
+ 
 // ------------ functions --------------
 // --- 달력 관련 ---
 function calendarMaker(target, date) {
-	 
-
+	let thisMonth = new Date(nowDate.getFullYear(), nowDate.getMonth(), 1);
+	let thisLastDay = new Date(nowDate.getFullYear(), nowDate.getMonth() + 1, 0);
+//	console.log("target : " + target);
+//	console.log("date : " + date);
 	if (date == null || date == undefined) {
 	    date = new Date();
 	}
 	nowDate = date;
-
+	
 	if ($(target).length > 0) {
 	    var year = nowDate.getFullYear();
 	    var month = nowDate.getMonth() + 1;
@@ -26,7 +27,7 @@ function calendarMaker(target, date) {
 	    return;
 	}
 	
-	var tag = "<tr>";
+	let tag = "<tr>";
 	var cnt = 0;
 	//빈 공백 만들어주기
 	for (i = 0; i < thisMonth.getDay(); i++) {
@@ -37,15 +38,12 @@ function calendarMaker(target, date) {
 	//날짜 채우기
 	for (i = 1; i <= thisLastDay.getDate(); i++) {
 	    if (cnt % 7 == 0) { tag += "<tr>"; }
-	
-	    if(i == nowDate.getDate()){
+		if(i == nowDate.getDate()){
 		    tag += "<td class='select_day'>" + i + "</td>";
-		} else{
+		} else {
 		    tag += "<td>" + i + "</td>";
 		}
-		
 	    cnt++;
-	    
 	    if (cnt % 7 == 0) {
 	        tag += "</tr>";
 	    }
@@ -53,8 +51,8 @@ function calendarMaker(target, date) {
 	
 	
 	//
-	calMoveEvtFn();
 	$(target).find("#customSetDate").append(tag);
+	calMoveEvtFn();
 }
 
 
@@ -62,13 +60,13 @@ function calMoveEvtFn() {
     // 전달 클릭
     $(".custom_calendar_table").on("click", ".prev", function () {
         nowDate = new Date(nowDate.getFullYear(), nowDate.getMonth() - 1, nowDate.getDate());
-		calendarMaker($('#calendarForm'), nowDate);
-		
+		calendarMaker($("#calendarForm"), nowDate);
+                
     });
     // 다음날 클릭
     $(".custom_calendar_table").on("click", ".next", function () {
         nowDate = new Date(nowDate.getFullYear(), nowDate.getMonth() + 1, nowDate.getDate());
-        calendarMaker($('#calendarForm'), nowDate);
+        calendarMaker($("#calendarForm"), nowDate);
     });
     // 일자 선택 클릭
     $(".custom_calendar_table").on("click", "td", function () {
@@ -102,82 +100,75 @@ function assembly(year, month) {
 	    "<tbody id='customSetDate' name='reservation_date'>" +
 	    "</tbody>" +
 	    "</table>";
-
+	
 	return calendar_html_code;
 }
 //- ------------------------------------------------
 // ---------- 날짜, 인원수 ajax로 체크 -----------
 function validationCheck(){
-	
-	// 연, 월, 일 저장
+        
+        // 연, 월, 일 저장
     let selectYear = $("#select_year").text();
     let selectMonth = $("#select_month").text();
     let selectDay = $(".select_day").text();
     
     // 연월일 합쳐서 저장
-    let selectedDay = selectYear + "-" +(("00" + selectMonth.toString()).slice(-2))+"-"+(("00" + selectDay.toString()).slice(-2)); // 연월일 불러오기
+	let selectedDay = selectYear + "-" +(("00" + selectMonth.toString()).slice(-2))+"-"+(("00" + selectDay.toString()).slice(-2)); // 연월일 불러오기
 	$("#reservation_date").val(selectedDay);
 	
 	// 인원수 저장
 	let count = $('input[name=reservation_person_count]:checked').val();
-	$("#reservation_person_count").val(count);	
-
-	let time = $('input[name=reservation_time]:checked').val();
-	$("#reservation_time").val(time);	
+	$("#reservation_person_count").val(count);        
 	
-//	let arr = [2, 4, 8];
+	let time = $('input[name=reservation_time]:checked').val();
+	$("#reservation_time").val(time);        
+	
 	let arr = dinningMax;
 	
 	if(arr.indexOf(Number(count)) == -1){
-		for (let cnt of arr) {
-		    if(count < cnt){
-		        count = cnt;
-		        break;
-		    }
-		}	
+        for (let cnt of arr) {
+            if(count < cnt){
+                count = cnt;
+                break;
+            }
+        }        
 	}
 	
-	switch(count){
-		case 2: $("#dinning_num").val(count); break;
-		case 4: $("#dinning_num").val(count); break;
-		case 8: $("#dinning_num").val(count); break;
-	}
-	
-	// 인원수 받아서 disable 걸기
+    // 인원수 받아서 disable 걸기
 	$.ajax({
 		type: "GET",
 		url: "ReservationSchedule",
 		data: {
-			'selectedDay' : selectedDay, // 예약날짜 reservation_date
-			'count' : count // 예약인원 reservation_person_count
+		        'selectedDay' : selectedDay,
+		        'count' : count
 		},
 		dataType: "JSON", // 리턴타입
 		success: function(data) { // 불가능 시간 배열
-
+		
 			// 전부 diabled 해제
 			$.each($("input[name=reservation_time]"), (i, e) => {
-				$(e).attr('disabled', false);	
+			        $(e).attr('disabled', false);        
 			})
 			$('input:radio[name=reservation_time]').eq(0).attr("checked", true);
 			
 			if(data.length < 1) return;
-
-			// 예약 불가 시간만 disabled true			
+			
+			// 예약 불가 시간만 disabled true                        
 			data.forEach((e, i) => {
-				let time = e.RESERVATION_TIME;
-				$.each($("input[name=reservation_time]"), (i2, e2) => {
-					if(time === e2.value){
-						$(e2).attr('disabled', true);
-					}
-				});
+		        let time = e.RESERVATION_TIME;
+		        $.each($("input[name=reservation_time]"), (i2, e2) => {
+		                if(time === e2.value){
+		                        $(e2).attr('disabled', true);
+		                }
+		        });
 			});
 		}
-	});	
+	});        
 }
 
 
 function emailEvtFn() {
-			
+                        
 	$("#reservation_email2").val($("#emailDomain").val());
 	
 	if($("#emailDomain").val() == "") { // 직접입력 선택 시
@@ -185,7 +176,7 @@ function emailEvtFn() {
 		$("#reservation_email2").css("background", ""); // 배경색 초기화
 		$("#reservation_email2").removeAttr("readonly"); // 읽기 전용 속성 제거(결과 동일)
 	} else {
-		// readonly 속성을 true 로 변경(읽기 전용)
+	        // readonly 속성을 true 로 변경(읽기 전용)
 		$("#reservation_email2").css("background", "lightgray"); // 배경색 초기화
 		$("#reservation_email2").attr("readonly", true); // 읽기 전용으로 변경
 	}
@@ -194,8 +185,8 @@ function emailEvtFn() {
  
  // ----------- DOM 랜더링 이후 ---------------
 $(function(){
-	calendarMaker($("#calendarForm"), new Date());	// 달력만들기
-	$("#emailDomain").change(emailEvtFn);	// 이메일 이벤트
-	$('.form_radio_btn1').on("change", validationCheck); // 인원수 이벤트
-	
-});
+	calendarMaker($("#calendarForm"), new Date());        // 달력만들기
+	$("#emailDomain").change(emailEvtFn);        // 이메일 이벤트
+	$('.form_radio_btn1').on("change", validationCheck()); // 인원수 이벤트
+})
+
